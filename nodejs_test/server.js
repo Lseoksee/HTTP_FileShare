a1a3a8b29e6c035d9ab5cb3e27fdfa03f33e1a19
Lseoksee <da864268@naver.com>
2023-05-28 12:44:42 +0900

    기본 서버 구성

---
+++ nodejs_test/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const server = express();
+const fs = require("fs");
+
+server.use(express.static(__dirname + "/../build"));
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/../build/static/js/main.8ee7bd76.js");
+});
+
+server.get("/getfile", (req, res) => {
+  fs.readdir(__dirname, (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.send(files);
+    }
+  });
+});
+
+server.listen(80, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 80");
+});
--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setState] = useState();
+
   return (
     <div>
-      
+      <a
+        href="/test.html"
+        onClick={(e) => {
+          e.preventDefault();
+
+          const Http = new XMLHttpRequest();
+
+          const url = "/getfile";
+
+          let temp = {};
+          Http.open("GET", url);
+          Http.send();
+          Http.onreadystatechange = (e) => {
+            console.log(Http.responseText.replace);
+          };
+        }}
+      >
+        파일확인하러 가기
+      </a>
     </div>
   );
 }
