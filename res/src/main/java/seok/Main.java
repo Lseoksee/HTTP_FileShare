0ae93403a8c46afeb6bd27a850b29e5600fcd57b
Lseoksee <da864268@naver.com>
2023-06-09 21:00:09 +0900

    서버 구동기 완성

--- res/src/main/java/seok/Main.java
+++ res/src/main/java/seok/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URI;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -16,31 +17,82 @@ import org.json.JSONObject;
 public class Main {
     static Process process;
     static Thread start;
+    static String outIpAddr;
+    static String inIpAddr;
+    static int port;
+    static Scanner sc;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         System.out.print("엔터키를 눌러 파일공유 서버를 작동시킵니다.");
         sc.nextLine();
         System.out.println("종료하려면 stop을 입력하세요.\n");
 
         try {
-            ProcessBuilder builder = new ProcessBuilder("node", "server.js");
-            builder.redirectErrorStream(true);
-            process = builder.start();
-            start = new Thread(new readTH());
-            start.start();
+            new Main().start();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void start() throws Exception {
+        ProcessBuilder builder = new ProcessBuilder("node", "server.js");
+        builder.redirectErrorStream(true);
+        process = builder.start();
+        start = new Thread(new readTH());
+        start.start();
 
         while (true) {
             String input = sc.nextLine().toLowerCase();
             if (input.equals("stop")) {
                 process.destroy();
                 break;
+            } else if (input.equals("info")) {
+                ipAddr();
+                continue;
+            } else if (input.equals("web")) {
+                Desktop.getDesktop().browse(new URI("http://" + outIpAddr));
+                continue;
+            } else if (input.equals("help")) {
+                System.out.println("stop:   서버작동을 중지합니다.");
+                System.out.println("info:   서버의 정보를 불러옵니다.");
+                System.out.println("web:    브라우저를 엽니다.");
             }
         }
         sc.close();
+    }
+
+    public synchronized void ipAddr() throws Exception {
+        if (outIpAddr != null) {
+            System.out.println("내부 ip주소: " + inIpAddr);
+            System.out.println("외부 ip주소: " + outIpAddr);
+            System.out.println("포트번호: "+ port);
+            return;
+        }
+        URL url = new URL("http://checkip.amazonaws.com/");
+        HttpURLConnection hur = (HttpURLConnection) url.openConnection();
+        BufferedReader ipbr = new BufferedReader(new InputStreamReader(hur.getInputStream()));
+
+        InetAddress ipAddress = InetAddress.getLocalHost();
+
+        FileInputStream fis = new FileInputStream("./server.json");
+        BufferedReader jsonbuffer = new BufferedReader(new InputStreamReader(fis));
+
+        JSONObject jsonObject = new JSONObject(jsonbuffer.lines().collect(Collectors.joining("\n")));
+        port = jsonObject.getInt("port");
+
+        if (port == 80 || port == 443) {
+            outIpAddr = ipbr.readLine();
+            inIpAddr = ipAddress.getHostAddress();
+        } else {
+            outIpAddr = ipbr.readLine() + ":" + port;
+            inIpAddr = ipAddress.getHostAddress();
+        }
+
+        System.out.println("내부 ip주소: " + inIpAddr);
+        System.out.println("외부 ip주소: " + outIpAddr);
+        Desktop.getDesktop().browse(new URI("http://" + outIpAddr));
+        jsonbuffer.close();
     }
 }
 
@@ -48,25 +100,7 @@ class readTH extends Main implements Runnable {
     @Override
     public void run() {
         try {
-            URL url = new URL("http://checkip.amazonaws.com/");
-            HttpURLConnection hur = (HttpURLConnection) url.openConnection();
-            BufferedReader ipbr = new BufferedReader(new InputStreamReader(hur.getInputStream()));
-
-            FileInputStream fis = new FileInputStream("./server.json");
-            BufferedReader jsonbuffer = new BufferedReader(new InputStreamReader(fis));
-
-            JSONObject jsonObject = new JSONObject(jsonbuffer.lines().collect(Collectors.joining("\n")));
-            int port = jsonObject.getInt("port");
-            String login;
-
-            if (port == 80 || port == 443)
-                login = ipbr.readLine();
-            else
-                login = ipbr.readLine() + ":" + port;
-
-            System.out.println("공유 ip 주소: " + login);
-            Desktop.getDesktop().browse(new URI("http://" + login));
-            jsonbuffer.close();
+            ipAddr();
 
             BufferedReader br = new BufferedReader(process.inputReader(Charset.forName("utf-8")));
             String line;
